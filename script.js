fc547c0f7c7e8ae4a3e0c90cee49684a11b55c27
anmolsood0007 <156082963+anmolsood0007@users.noreply.github.com>
2024-03-17 14:48:31 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    // When the page fully loads, hide the loading animation
+    var loadingContainer = document.querySelector('.loading-container');
+    loadingContainer.style.display = 'none';
+  });
